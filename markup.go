e8d2633e9dc6462e4c6e380f78332109dbfdbd6e
Thomas Ziegler <thomas.zglr@googlemail.com>
2022-06-27 13:58:34 +0200

    fix double usage of markups

--- markup.go
+++ markup.go
@@ -78,42 +78,42 @@ func (mp *MarkupParser) ParseAll(runner Runner) (string, error) {
 }
 
 func (mark *MarkupEntry) ExecParse(runner Runner, current string, stopOnErrors bool) (string, error) {
-	var results []string
+	output := ""
 	for _, mup := range mark.Properties {
 
 		if runner, exists := runner.runners[mup.Name]; exists {
-			add := runner.Exec(mup, current)
-			results = append(results, add)
+			output = runner.Exec(mup, output)
 		} else {
 			if stopOnErrors {
 				return "", errors.New("undefined propertie: " + mup.Name)
-			} else {
-				results = append(results, "[ERROR] unknown markup")
 			}
 		}
 	}
-	return strings.Join(results, ""), nil
+	return output, nil
 }
 
+// AddRunner applies the logic to an markup
 func (r *Runner) AddRunner(responsible string, runner MarkupRunner) error {
-	if _, exists := r.runners[responsible]; exists {
+	if _, exists := r.runners[responsible]; exists { // we do not allow overwrite existing runners
 		return errors.New("runner already exists for " + responsible)
 	}
 	r.runners[responsible] = runner
 	return nil
 }
 
-func (r *Runner) getMarkups(orig string, ref string) []Markup {
-	var marks []Markup
-	orig = strings.TrimLeft(orig, string(r.leftBracket))
-	orig = strings.TrimRight(orig, string(r.rightRBracket))
-	parts := strings.Split(orig, string(r.level1Sep))
+// getMarkups parses the markup string itself to get all sub definitions within the markup
+// any markup can contains multiple 'real' markusp,that seperated by the level1 seperator
+func (r *Runner) getMarkups(markupAsString string, ref string) []Markup {
+	var marks []Markup                                                          // this will be oure result
+	markupAsString = strings.TrimLeft(markupAsString, string(r.leftBracket))    // remove the markup strings. on left
+	markupAsString = strings.TrimRight(markupAsString, string(r.rightRBracket)) // and right
+	parts := strings.Split(markupAsString, string(r.level1Sep))                 // first spits by the level1 seperator. it splits the markups itself
 	for _, pstr := range parts {
-		pParts := strings.Split(pstr, string(r.level2Sep))
+		pParts := strings.Split(pstr, string(r.level2Sep)) // split again by using the second sperarator. that is usual an assigment like =
 		var m Markup
-		m.Name = pParts[0]
-		m.Reference = ref
-		if len(pParts) > 1 {
+		m.Name = pParts[0]   // the name of the markup is always the left side.
+		m.Reference = ref    // this is the context. meaning the string that needs to be woking on
+		if len(pParts) > 1 { // if we have different assignements (by split using level2sep), then we assign them too
 			m.Value = pParts[1]
 		}
 		marks = append(marks, m)
@@ -137,20 +137,19 @@ func (r *Runner) ParseMarkup(orig string) MarkupParser {
 			return parsed
 		}
 	}
-	// first extract the markups, and iterate over them, if we found some
-	if markups, found := r.splitByMarks(orig); found {
+
+	searchString := orig                               // we need a copy of the origin string, so we can cut them after any search hit
+	if markups, found := r.splitByMarks(orig); found { // first extract the markups, and iterate over them, if we found some
 		allMk := len(markups)
-		// simple iterate over all markups
-		for mkIndex, mk := range markups {
-			// get the postion of the markup in the orogin string
-			if indxStart := strings.Index(orig, mk); indxStart >= 0 {
-				//at the first markup, we need also keep the left side of the origin string
-				if mkIndex == 0 {
-					parsed.LeftString = orig[:indxStart]
+
+		for mkIndex, mk := range markups { // simple iterate over all markups
+
+			if indxStart := strings.Index(searchString, mk); indxStart >= 0 { // get the postion of the markup in the orogin string
+				if mkIndex == 0 { //at the first markup, we need also keep the left side of the origin string
+					parsed.LeftString = searchString[:indxStart] // keep the left side
 				}
-				// the length of the markup string.
-				// we need them for some calculations depending the string indecies
-				offset := len(mk)
+				offset := len(mk) // the length of the markup string. we need them for some calculations depending the string indecies
+
 				text := ""
 				// the right part of the string
 				// would have no markup, so all the
@@ -158,18 +157,16 @@ func (r *Runner) ParseMarkup(orig string) MarkupParser {
 				// any other part of the text is handled
 				// in the else part
 				if mkIndex+1 < allMk {
-					if end := strings.Index(orig[indxStart+offset:], string(r.leftBracket)); end >= 0 {
-						text = orig[indxStart+offset : indxStart+end+offset]
+					if end := strings.Index(searchString[indxStart+offset:], string(r.leftBracket)); end >= 0 {
+						text = searchString[indxStart+offset : indxStart+end+offset]
 					}
 				} else {
-					text = orig[indxStart+offset:]
+					text = searchString[indxStart+offset:]
 				}
-				// create a new markup entry.
-				// do not overlook r.getMarkups because
-				// there we parsing the markup sub enties
-				var entry MarkupEntry = MarkupEntry{
+				searchString = searchString[indxStart+offset:]
+				var entry MarkupEntry = MarkupEntry{ // create a new markup entry.
 					Text:       text,
-					Properties: r.getMarkups(mk, text),
+					Properties: r.getMarkups(mk, text), // do not overlook r.getMarkups because there we parsing the markup sub enties
 				}
 				parsed.Entries = append(parsed.Entries, entry)
 			}
@@ -183,15 +180,18 @@ func (r *Runner) ParseMarkup(orig string) MarkupParser {
 	return parsed
 }
 
-// splitByMarks extract all markups parts
+// splitByMarks extract all parts of the text, that
+// matched to the pattern. that means it starts
+// with the char leftBracket, have some content, and ends with
+// the rightBracket.
+// these two bracket definitions the only nes we used here
 func (r *Runner) splitByMarks(orig string) ([]string, bool) {
-
 	var result []string
 	found := false
-	cmpStr := string(r.leftBracket) + "[^" + string(r.rightRBracket) + "]+" + string(r.rightRBracket)
-	re := regexp.MustCompile(cmpStr)
-	newStrs := re.FindAllString(orig, -1)
-	for _, s := range newStrs {
+	cmpStr := string(r.leftBracket) + "[^" + string(r.rightRBracket) + "]+" + string(r.rightRBracket) // compose the regex
+	re := regexp.MustCompile(cmpStr)                                                                  // compile and panic on bad things happens
+	newStrs := re.FindAllString(orig, -1)                                                             // use regex to find all patterns
+	for _, s := range newStrs {                                                                       // get all markups
 		found = true
 		result = append(result, s)
 
--- markup_test.go
+++ markup_test.go
@@ -26,13 +26,13 @@ func TestRegularCreate(t *testing.T) {
 	runParser.AddRunner("parse", markupstring.MarkupRunner{
 		Exec: func(mk markupstring.Markup, current string) string {
 			parseIsExeucted++
-			return "-->" + strings.ToUpper(mk.Reference) + "<--"
+			return "[P]" + strings.ToUpper(mk.Reference) + "[/P]"
 		},
 	})
 
 	runParser.AddRunner("mod", markupstring.MarkupRunner{
 		Exec: func(mk markupstring.Markup, current string) string {
-			return "-->" + strings.ToLower(mk.Reference) + "<--"
+			return "[M]" + strings.ToLower(mk.Reference) + "[/M]"
 		},
 	})
 
@@ -43,7 +43,45 @@ func TestRegularCreate(t *testing.T) {
 		t.Error(rErr)
 	} else {
 		fmt.Println(result)
-		if result != "1.Regular Start...-->2.OTHER STRING<---->...3.the rest<--" {
+		if result != "1.Regular Start...[P]2.OTHER STRING[/P][M]...3.the rest[/M]" {
+			t.Error("unexecpected result:[", result, "]")
+		}
+
+		if parseIsExeucted != 1 {
+			t.Error("parse is not executed once. ", parseIsExeucted)
+		}
+	}
+
+}
+
+func TestRegularCreateDoubleMarkups(t *testing.T) {
+	runParser, err := markupstring.NewRunner(true, "<>;=")
+
+	if err != nil {
+		t.Error(err)
+	}
+	parseIsExeucted := 0
+	runParser.AddRunner("parse", markupstring.MarkupRunner{
+		Exec: func(mk markupstring.Markup, current string) string {
+			parseIsExeucted++
+			return "[P]" + strings.ToUpper(mk.Reference) + "[/P]"
+		},
+	})
+
+	runParser.AddRunner("mod", markupstring.MarkupRunner{
+		Exec: func(mk markupstring.Markup, current string) string {
+			return "[M]" + strings.ToLower(mk.Reference) + "[/M]"
+		},
+	})
+
+	toParse := "1.Regular Start...<parse><mod>2.other string<mod>...3.the rest"
+
+	parser := runParser.ParseMarkup(toParse)
+	if result, rErr := parser.ParseAll(*runParser); rErr != nil {
+		t.Error(rErr)
+	} else {
+		fmt.Println(result)
+		if result != "1.Regular Start...[P][/P][M]2.other string[/M][M]...3.the rest[/M]" {
 			t.Error("unexecpected result:[", result, "]")
 		}
 
